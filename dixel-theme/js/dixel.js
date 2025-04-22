798c7dfd158714832aeb37ff3311935bc8227b35
Dymedrol <dmitrykob@gmail.com>
2025-04-22 21:08:07 +0700

    big slider update

--- dixel-theme/js/dixel.js
+++ dixel-theme/js/dixel.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+    // burger menu
     const burgerButton = $('#header-burger');
     const mobileMenu = $('#header-mobile-menu');
     const mobileMenuCloseButton = $('#header-mobile-close');
@@ -9,5 +11,34 @@ $(document).ready(function() {
 
     mobileMenuCloseButton.click(function() {
         mobileMenu.css('display', 'none');
+    });
+
+    // our services slider
+
+    $('.dixel-our-services-content').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        dots: false,
+        mobilefirst: true,
+        prevArrow: $('.dixel-our-services-prev'),
+        nextArrow: $('.dixel-our-services-next'),
+        responsive: [
+            {
+                breakpoint: 9999,
+                settings: "unslick"
+            },
+            {
+                breakpoint: 720,
+                settings: {
+                    slidesToShow: 1,
+                    variableWidth: true,
+                    customPaging: 20,
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
     });
 });
